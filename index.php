8758a1857ffbc83788870fb980625caf31411335
Dotan Cohen <github-com-2010.04@dotancohen.com>
2016-10-06 17:54:27 +0300

    Remove exception that could interfere with other autoloaders

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ spl_autoload_register(function($class) {
 	$file = "/classes/{$class}.php";
 	if ( file_exists($file) && is_readable($file) ) {
 		require_once($file);
-	} else {
-		throw new \Exception("Please upload WordSquash files to the server.").
 	}
 });
 
